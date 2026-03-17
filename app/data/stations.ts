1c51c382a2c6607828144f2d85c3e8f7ccf16c24
Skydomeblue dev <skydomeblue.dev@gmail.com>
2026-03-18 01:09:36 +0700

    update st

--- app/data/stations.ts
+++ app/data/stations.ts
@@ -35,9 +35,16 @@ export const stations = [
     nowPlayingApi: "",
   },
   {
-    id: 5,
+    id: 6,
     name: "Firza Radio Padang",
     stream: "https://stream.zeno.fm/5k7g2w3gtkeuv",
+    logo: "/radio.png",
+    nowPlayingApi: "",
+  },
+  {
+    id: 7,
+    name: "Rumah Gadang Saiyo",
+    stream: "https://stream.zeno.fm/nuhda9xayrbtv",
     logo: "/radio.png",
     nowPlayingApi: "",
   },
